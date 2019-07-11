5999fbcc5277715bb880b8e87933e49c50cd8def
Jeki Kit <kit.jrs@gmail.com>
2019-07-11 16:42:22 +0700

    update

--- next.config.js
+++ next.config.js
@@ -1,10 +1,24 @@
-module.exports = {
-  webpack: config => {
-    // Fixes npm packages that depend on `fs` module
-    config.node = {
-      fs: 'empty'
-    }
+import withCSS from '@zeit/next-css';
+import withSass from '@zeit/next-sass';
+import withLess from '@zeit/next-less';
 
-    return config
+const isProd = process.env.NODE_ENV === 'production'
+
+// fix: prevents error when .less files are required by node
+if (typeof require !== 'undefined') {
+  require.extensions['.less'] = file => { }
+}
+
+export default withLess(withSass(withCSS({
+  lessLoaderOptions: {
+    javascriptEnabled: true
   }
+})))
+
+export function webpack(config) {
+  // Fixes npm packages that depend on `fs` module
+  config.node = {
+    fs: 'empty'
+  };
+  return config;
 }
